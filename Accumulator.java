1d5788ebc635eec195c20f2ae798aa99c3f579e0
JasonZheng02 <Jzheng02@stuy.edu>
2019-03-18 00:10:36 -0400

    Finished method catElementsStartingWith

--- Accumulator.java
+++ Accumulator.java
@@ -16,78 +16,85 @@ public class Accumulator {
       ) {
         String result = "";
 
-        for( int el = 0; el < list.size(); el++)
-            result += // dummy for incremental development
-                      list.get( el) + " ";
+        for( int el = 0; el < list.size(); el++){
 
             /* 3.  Stumbling block 0
                Java protects a programmer against applying a method to
                elements in list when some elements of the list might
                omit support for a particular operation.
 
-               This protection is implemented by the ___________ (compiler? JVM?)
+               This protection is implemented by the compiler
                The following code violates the restriction:
               */
-            // [code that violates the restriction]
-
+            // if (list.get(el).startsWith(prefix))
+            //     result += list.get(el);}
+            
             /*
              predicted error message:
-
+                list.get(el)'s object type can be something that startsWith can not support
              actual error message:
-             */
+                cannot find symbol
+                if (list.get(el).startsWith(prefix))
+                                ^
+                symbol:   method startsWith(String)
+                location: class Object
 
+             */
 
             /* 4.  Workaround 0
                A programmer should expect there to be a way to
                work around the stumbling block, because
-               the ___________ (compiler? JVM?)
-               knows the type of an element.
+               the JVM knows the type of an element.
 
-               Java's ___________ operator identifies the type
-               of an element to the ___________ (compiler? JVM?).
+               Java's instanceof operator identifies the type
+               of an element to the JVM.
              */
-
 
             /* 5.  Stumbling block 1
                However, use of the operator alone is insufficient,
-               because the ___________ (compiler? JVM?)
+               because the JVM
                objects to the following code that adds use of
                the operator to the code from Stumbling block 0:
              */
 
-            // [code that illustrates the use of the operator]
-
+             // if (list.get(el) instanceof String){
+                 // if (list.get(el).startsWith(prefix))
+                    // result += list.get(el);}
+            // }   
             /*
              predicted error message:
-
+                Same as before
              actual error message:
+                Same as before
              */
 
-
             /* 6. Workaround 1
-               Programmers use Java's _____________ operator
-               to tell the ___________ (compiler? JVM?)
+               Programmers use Java's cast operator
+               to tell the compiler
                that code uses a subclass's method on an object,
                even though the reference to the object is stored
                in a super-class variable.
              */
 
-
             /* 7. Stumbling block 2
                However, use of this operator alone is insufficient,
-               because the ___________ (compiler? JVM?)
+               because the JVM
                objects to the following code that adds use of
                the operator to the code from Stumbling block 0:
              */
 
-            // [code that illustrates the use of the operator]
-
+             // if (((String)list.get(el)).startsWith(prefix))
+                 // result += list.get(el);    
+            // }
             /*
              predicted error message:
-
+                Double or Integer can not be casted to String
              actual error message:
+                Exception in thread "main" java.lang.ClassCastException: 
+                java.base/java.lang.Double cannot be cast to java.base/java.lang.String
+                at Accumulator.catElementsStartingWith(Accumulator.java:86)
+                at UserOfList.main(UserOfList.java:32)
              */
-
 
             /* 8. Workaround 2
                A programmer can combine use of both operators
@@ -95,8 +102,11 @@ public class Accumulator {
                elements in the list that support the method.
              */
 
-            // [working code here, finally]
-
+            if (list.get(el) instanceof String){
+                if (((String)list.get(el)).startsWith(prefix))
+                    result += list.get(el) + " ";
+            }
+        }
         return result;
     }
 
